d5f36758eea7aa11ec07c47bcc5a84bc82b94381
Sergei Romanenko <sergei.romanenko@supercompilers.ru>
2011-09-11 14:36:28 +0400

    Initial commit.

---
+++ mrsc-trs2/src/main/scala/mrsc/trs2/language.scala
@@ -0,0 +1,12 @@
+package mrsc.trs2
+
+trait TRSSyntax[C] {
+  def rebuildings(c: C): List[C]
+  def equiv(c1: C, c2: C): Boolean 
+  def instanceOf(c1: C, c2: C): Boolean
+}
+
+// There are n rules. Variants should return a list of length n
+trait RewriteSemantics[C] {
+  def driveConf(c: C): List[Option[C]]
+}
